fcbc02f7c277c5b337efa903f47c51e4a52c3555
wapenshaw <thescarletcars@gmail.com>
2020-08-02 01:54:35 +0530

    Updates

---
+++ precache-manifest.1d68b27b8af0b727952909c541ef42de.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bac636f8f09517b20fc2c8660d7616d",
+    "url": "/react-minis/index.html"
+  },
+  {
+    "revision": "a3fcb1278750c60c681d",
+    "url": "/react-minis/static/css/main.c490222f.chunk.css"
+  },
+  {
+    "revision": "08e819fa97fe96757e54",
+    "url": "/react-minis/static/js/2.38e9cc0f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-minis/static/js/2.38e9cc0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3fcb1278750c60c681d",
+    "url": "/react-minis/static/js/main.1f071c86.chunk.js"
+  },
+  {
+    "revision": "f58cc738ca0adf67391e",
+    "url": "/react-minis/static/js/runtime-main.20d32627.js"
+  },
+  {
+    "revision": "dba15ec7d2ae99850f54e75b334a931d",
+    "url": "/react-minis/static/media/flip.dba15ec7.png"
+  },
+  {
+    "revision": "e993aab53ec42dd3db016b5098d0bdd7",
+    "url": "/react-minis/static/media/heads.e993aab5.png"
+  },
+  {
+    "revision": "fda23f877ea3589eafaff51dadf09f07",
+    "url": "/react-minis/static/media/tails.fda23f87.png"
+  }
+]);
